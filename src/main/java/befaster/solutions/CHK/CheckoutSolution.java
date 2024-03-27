37e727ed05980677ba2e57516d6e4e5ec452e473
sbx_user1051 <sbx_user1051@169.254.54.77>
2024-03-27 17:11:37 +0000

    Wed Mar 27 17:11:37 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -2,12 +2,27 @@ package befaster.solutions.CHK;
 
 import befaster.runner.SolutionNotImplementedException;
 
+import java.util.List;
+
 public class CheckoutSolution {
     public Integer checkout(String skus) {
+        String[] lineParts = skus.split(",");
+        Integer sum = 0;
+        for(int i = 0; i < lineParts.length; i+=3){
+            SKU sku = new SKU(lineParts[i], Integer.parseInt(lineParts[i+1]), lineParts[i+2]);
+
+            sum += calculateItemPrice(sku);
+        }
 
         return 0;
 
     }
 
+    private Integer calculateItemPrice(SKU sku) {
+        String[] splitted = sku.specialOffer().split(" for ");
+        Integer qty = splitted[0].substring()
+    }
+
     record SKU(String Item, int price, String specialOffer){}
 }
+
--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -10,7 +10,10 @@ class CheckoutSolutionTest {
     CheckoutSolution solution;
 
     public static String exampleSKU = """
-            
+            | A    | 50    | 3A for 130     |
+            | B    | 30    | 2B for 45      |
+            | C    | 20    |                |
+            | D    | 15    |                |
             """;
     @BeforeEach
     void setUp() {
