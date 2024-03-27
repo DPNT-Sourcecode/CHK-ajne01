37caa92ab5d27a09c802390979f7a8366d4a4560
sbx_user1051 <sbx_user1051@169.254.54.77>
2024-03-27 18:28:55 +0000

    Wed Mar 27 18:28:55 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -17,92 +17,97 @@ public class CheckoutSolution {
             return -1;
         }
 
-        Map<String, Integer> itemsMap = new HashMap<>();
+        Map<String, Integer> countItem = new HashMap<>();
 
-        itemsMap.put("A", skus.length() - skus.replace("A", "").length());
-        itemsMap.put("B", skus.length() - skus.replace("B", "").length());
-        itemsMap.put("C", skus.length() - skus.replace("C", "").length());
-        itemsMap.put("D", skus.length() - skus.replace("D", "").length());
-        itemsMap.put("E", skus.length() - skus.replace("E", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("G", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("H", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("I", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("J", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
-        itemsMap.put("F", skus.length() - skus.replace("F", "").length());
+        countItem.put("A", skus.length() - skus.replace("A", "").length());
+        countItem.put("B", skus.length() - skus.replace("B", "").length());
+        countItem.put("C", skus.length() - skus.replace("C", "").length());
+        countItem.put("D", skus.length() - skus.replace("D", "").length());
+        countItem.put("E", skus.length() - skus.replace("E", "").length());
+        countItem.put("F", skus.length() - skus.replace("F", "").length());
+        countItem.put("G", skus.length() - skus.replace("G", "").length());
+        countItem.put("H", skus.length() - skus.replace("H", "").length());
+        countItem.put("I", skus.length() - skus.replace("I", "").length());
+        countItem.put("J", skus.length() - skus.replace("J", "").length());
+        countItem.put("K", skus.length() - skus.replace("K", "").length());
+        countItem.put("L", skus.length() - skus.replace("L", "").length());
+        countItem.put("M", skus.length() - skus.replace("M", "").length());
+        countItem.put("N", skus.length() - skus.replace("N", "").length());
+        countItem.put("O", skus.length() - skus.replace("O", "").length());
+        countItem.put("P", skus.length() - skus.replace("P", "").length());
+        countItem.put("Q", skus.length() - skus.replace("Q", "").length());
+        countItem.put("R", skus.length() - skus.replace("R", "").length());
+        countItem.put("S", skus.length() - skus.replace("S", "").length());
+        countItem.put("T", skus.length() - skus.replace("T", "").length());
+        countItem.put("U", skus.length() - skus.replace("U", "").length());
+        countItem.put("V", skus.length() - skus.replace("V", "").length());
+        countItem.put("X", skus.length() - skus.replace("X", "").length());
+        countItem.put("W", skus.length() - skus.replace("W", "").length());
+        countItem.put("Y", skus.length() - skus.replace("Y", "").length());
+        countItem.put("Z", skus.length() - skus.replace("Z", "").length());
 
 
 
         Integer sum = 0;
 
-        while(countE > 0){
-            if(countE >= 2){
-                Integer count = (int) Math.floor((double) countE / 2);
+        while(countItem.get("E") > 0){
+            if(countItem.get("E") >= 2){
+                Integer count = (int) Math.floor((double) countItem.get("E") / 2);
                 sum += count*2*40;
-                countB -= count;
-                countE -= count*2;
+                countItem.replace("B", countItem.get("B") - count);
+                countItem.replace("E", countItem.get("E") - count*2);
             } else {
-                sum += countE*40;
-                countE=0;
+                sum += countItem.get("E")*40;
+                countItem.replace("E", 0);
             }
         }
 
 
-        while (countA > 0) {
-            if(countA >= 5){
-                int quantPromo = (int) Math.floor((double) countA / 5);
+        while (countItem.get("A") > 0) {
+            if(countItem.get("A") >= 5){
+                int quantPromo = (int) Math.floor((double) countItem.get("A") / 5);
                 sum += quantPromo*200;
-                countA -= quantPromo*5;
-            } else if(countA >= 3){
-                int quantPromo = (int) Math.floor((double) countA / 3);
+                countItem.replace("A", countItem.get("A") - quantPromo*5);
+            } else if(countItem.get("A") >= 3){
+                int quantPromo = (int) Math.floor((double) countItem.get("A") / 3);
                 sum += quantPromo*130;
-                countA -= quantPromo*3;
+                countItem.replace("A", countItem.get("A") - quantPromo*3);
             } else {
-                sum += 50 * countA;
-                countA = 0;
+                sum += 50 * countItem.get("A");
+                countItem.replace("A", 0);
             }
         }
-        while(countB>0) {
-            if (countB >= 2) {
-                int quantPromo = (int) Math.floor((double) countB / 2);
+        while(countItem.get("B")>0) {
+            if (countItem.get("B") >= 2) {
+                int quantPromo = (int) Math.floor((double) countItem.get("B") / 2);
                 sum += quantPromo * 45;
-                countB = countB - quantPromo * 2;
+                countItem.replace("B", countItem.get("B") - countItem.get("B") - quantPromo * 2);
             } else {
-                sum += 30 * countB;
-                countB = 0;
+                sum += 30 * countItem.get("B");
+                countItem.replace("B", 0);
             }
         }
 
-        if(countC >0){
-            sum += 20 * countC;
+        if(countItem.get("C") >0){
+            sum += 20 * countItem.get("C");
         }
 
-        if(countD >0){
-            sum += 15 * countD;
+        if(countItem.get("D") >0){
+            sum += 15 * countItem.get("D");
         }
 
-        while(countF >= 3){
+        while(countItem.get("F") >= 3){
             sum += 20;
-            countF -= 3;
+            countItem.replace("F", countItem.get("F") - 3);
         }
 
-        if(countF > 0){
-            sum += 10 * countF;
-            countF = 0;
+        if(countItem.get("F") > 0){
+            sum += 10 * countItem.get("F");
+            countItem.replace("F", 0);
         }
 
         return sum;
 
     }
 }
+
