09fd9366f207458a1063e9137c03dc130fd81ddf
sbx_user1051 <sbx_user1051@169.254.54.77>
2024-03-27 17:30:34 +0000

    Wed Mar 27 17:30:34 UTC 2024

--- src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
+++ src/test/java/befaster/solutions/CHK/CheckoutSolutionTest.java
@@ -9,7 +9,10 @@ class CheckoutSolutionTest {
 
     CheckoutSolution solution;
 
-    public static String sku100 = "ABC";
+    public static String sku100ABC = "ABC";
+    public static String sku130AAA = "AAA";
+    public static String sku45BB = "BB";
+    public static String sku175AAABB = "AAABB";
     @BeforeEach
     void setUp() {
         solution = new CheckoutSolution();
@@ -17,7 +20,10 @@ class CheckoutSolutionTest {
 
     @Test
     void checkout() {
-        assertEquals(100, solution.checkout(sku100));
+        assertEquals(100, solution.checkout(sku100ABC));
+        assertEquals(130, solution.checkout(sku130AAA));
+        assertEquals(45, solution.checkout(sku45BB));
+        assertEquals(175, solution.checkout(sku175AAABB));
 
     }
 }
