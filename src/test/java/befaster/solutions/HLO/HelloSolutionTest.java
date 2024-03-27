88d23d21dfadaba6427d263908b3f213513b026f
sbx_user1051 <sbx_user1051@169.254.54.77>
2024-03-27 16:51:57 +0000

    Wed Mar 27 16:51:57 UTC 2024

--- src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
+++ src/test/java/befaster/solutions/HLO/HelloSolutionTest.java
@@ -1,4 +1,23 @@
+package befaster.solutions.HLO;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+
 class HelloSolutionTest {
-  
+
+    HelloSolution solution;
+
+    @BeforeEach
+    void setUp(){
+        solution = new HelloSolution();
+    }
+
+    @Test
+    void testHello(){
+            assertFalse(solution.hello("Me").isEmpty());
+    }
+
 }
